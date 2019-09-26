573025f568f49fb63a585dba0b9b36a1d70da631
darafitteron <def223@lehigh.edu>
2019-09-26 19:34:38 -0400

    Edited Header

--- Intro to R.R
+++ Intro to R.R
@@ -1,9 +1,11 @@
-# R script to accopany Intro to R
-# Copy for RStudent 2019
+# R script to accompany Intro to R
+# Copy for Dara Fitteron
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
 # in your script by putting "#" at the beginning of the lines.
-# 
+# Hi Im Cool
+# Lehigh University
+# :)
 # In fact, you can put comments anywhere on a line as long as 
 # you put "#" before them.
